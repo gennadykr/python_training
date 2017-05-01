e61c8e07914b0c196fd7a6b18f4475e6d2e83f01
Gennady Krasnikov <gennady.krasnikov@gmail.com>
2017-05-01 03:10:05 +0300

    Задание №6: Сделать фикстуру общей для всех тестов

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,16 +1,5 @@
-import time
-
-import pytest
-
-from fixture.application import Application
 from model.contact import Contact
-
-
-@pytest.fixture
-def app(request):
-    fixture = Application()
-    request.addfinalizer(fixture.destroy)
-    return fixture
+import time
 
 
 def test_add_contact(app):
