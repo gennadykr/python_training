8dfcf35a04557bec4f0efcd87403cd56c864db64
Gennady Krasnikov <gennady.krasnikov@gmail.com>
2017-05-31 04:29:58 +0300

    Задание №16: Реализовать опции командной строки, позволяющие указывать тип браузера и адрес тестируемого приложения

--- conftest.py
+++ conftest.py
@@ -4,13 +4,15 @@ from fixture.application import Application
 fixture = None
 
 @pytest.fixture
-def app():
+def app(request):
     global fixture
+    browser = request.config.getoption("--browser")
+    base_url = request.config.getoption("--base_url")
     if fixture is None:
-        fixture = Application()
+        fixture = Application(browser=browser, base_url=base_url)
     else:
         if not fixture.is_valid():
-            fixture = Application()
+            fixture = Application(browser=browser, base_url=base_url)
     fixture.session.ensure_login(username="admin", password="secret")
     return fixture
 
@@ -22,3 +24,8 @@ def stop(request):
         fixture.destroy()
     request.addfinalizer(fin)
     return fixture
+
+def pytest_addoption(parser):
+    parser.addoption("--browser", action="store", default="chrome")
+    parser.addoption("--base_url", action="store", default="http://localhost/addressbook/")
+
