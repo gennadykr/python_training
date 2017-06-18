45eabc3e55c773ba6e931949b27ba672fe8aaaad
Gennady Krasnikov <gennady.krasnikov@gmail.com>
2017-06-18 14:42:52 +0300

    Separated test data for group

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,20 +1,6 @@
 from model.group import Group
 import pytest
-import random
-import string
-
-def random_string(prefix, maxlen):
-    # + string.punctuation
-    # " " in the end of name lead to error too!
-    symbols = string.ascii_letters + string.digits + " "*10
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
-
-
-testdata = [Group(name="", header="", footer="")] + [
-    Group(name=random_string("name",10), header=random_string("header",20), footer=random_string("footer",20))
-    for i in range(5)
-]
-
+from data.add_group import constant as testdata
 
 @pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
 def test_add_group(app, group):
