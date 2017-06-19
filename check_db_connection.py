1dff08a853199ab5db314bf15bba92c9dd2247c0
Gennady Krasnikov <gennady.krasnikov@gmail.com>
2017-06-19 04:49:18 +0300

    ORM DB relations works

--- check_db_connection.py
+++ check_db_connection.py
@@ -1,3 +1,5 @@
+from model.group import Group
+
 #import pymysql.cursors
 #connection = pymysql.connect(host="127.0.0.1", database="addressbook", user="root", password="")
 # try:
@@ -14,10 +16,26 @@ from fixture.orm import ORMFixture
 db = ORMFixture(host="127.0.0.1", name="addressbook", user="root", password="")
 
 
+# try:
+#     l = db.get_contact_list()
+#     for item in l:
+#         print(item)
+#     print(len(l))
+# finally:
+#     pass
+
 try:
-    l = db.get_contact_list()
+    l = db.get_contacts_in_group(Group(id="19"))
     for item in l:
         print(item)
     print(len(l))
 finally:
     pass
+
+# try:
+#     l = db.get_contacts_not_in_group(Group(id="19"))
+#     for item in l:
+#         print(item)
+#     print(len(l))
+# finally:
+#     pass
